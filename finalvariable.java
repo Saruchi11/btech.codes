8314eb67afd9afb079d9039443052f29a07caacc
Saruchi11 <saruchi1008@gmail.com>
2026-05-17 00:59:02 +0530

    finalvariable

---
+++ finalvariable.java
@@ -0,0 +1,15 @@
+class Demo {
+    final int x = 100;
+
+    void show() {
+        System.out.println(x);
+    }
+}
+
+public class finalvariable {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        d.show();
+    }
+}
+
