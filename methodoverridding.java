52d94cfcf56005d7e28d230639bc584aa9945eda
Saruchi11 <saruchi1008@gmail.com>
2026-05-17 02:32:01 +0530

    methodoverriding

---
+++ methodoverridding.java
@@ -0,0 +1,18 @@
+class Parent {
+    void show() {
+        System.out.println("Parent");
+    }
+}
+
+class Child extends Parent {
+    void show() {
+        System.out.println("Child");
+    }
+}
+
+public class methodoverridding {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.show();
+    }
+}
