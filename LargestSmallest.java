1c4851fc54604f667fb0f1dc906fefd93b5f0bbf
Saruchi11 <saruchi1008@gmail.com>
2026-05-17 02:21:03 +0530

    LargestSmallest

---
+++ LargestSmallest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LargestSmallest {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int largest, smallest;
+    largest = a;
+    smallest = a;
+    if (b> largest);
+    largest = b;
+    if (c> largest);
+    largest = c;
+    if (b< smallest);
+    smallest = b;
+    if (c< smallest);
+    smallest = c;
+    System.out.println("largest =" +largest);
+    System.out.println("smallest=" +smallest);
+
+
+    
+    }
+    
+}
