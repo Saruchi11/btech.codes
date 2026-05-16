196b22ca81c72f3c85e33c377eafb5e6952b74a3
Saruchi11 <saruchi1008@gmail.com>
2026-05-17 02:40:47 +0530

    numbercheckdiv3and5

---
+++ numbercheckdiv3and5.java
@@ -0,0 +1,12 @@
+class numbercheckdiv3and5 {
+    public static void main(String[] agrs){
+        int n=50;
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0&&i%5==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
