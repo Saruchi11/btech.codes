4f604ad269efcf6fa310eb9241f45ed92472a551
Saruchi11 <saruchi1008@gmail.com>
2026-05-17 01:03:40 +0530

    frequency_of_num

---
+++ frequency_of_num.java
@@ -0,0 +1,17 @@
+public class frequency_of_num {
+    public static void main(String[] args){
+        int num=1223334444;
+        int[] freq = new int[10];
+        while(num!=0){
+            int rem=num%10;
+            freq[rem]++;
+            num /= 10;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>0){
+                System.out.println("Digit: " + i + ", Frequency: " + freq[i]);
+            }
+        }
+    }
+}
+
