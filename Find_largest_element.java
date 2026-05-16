4099f11b38d6eac8abe75c62ae4c3e200d3e10f4
Saruchi11 <saruchi1008@gmail.com>
2026-05-17 01:00:34 +0530

    Find_largest_element

---
+++ Find_largest_element.java
@@ -0,0 +1,16 @@
+public class Find_largest_element {
+  
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int largest=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        System.out.println("The largest element is: "+largest);
+    }
+}
+
+
+
