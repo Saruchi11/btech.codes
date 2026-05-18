ca4b9e7ee5f11ff94c5f2e244c67e992054b1016
Saruchi11 <saruchi1008@gmail.com>
2026-05-18 10:16:50 +0530

    SwapWithoutTemp

---
+++ SwapWithoutTemp.java
@@ -0,0 +1,22 @@
+ import java.util.Scanner;
+
+ public class SwapWithoutTemp{
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the first number ");
+    int a = sc.nextInt();
+    System.out.println("enter the second number");
+    int b = sc.nextInt();
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    
+    System.out.println("after swapping");
+    System.out.println("first number = "+a);
+    System.out.println("second number = " +b);
+
+    }
+
+    
+}
