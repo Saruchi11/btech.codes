db4805bb05f41d6736695b114121bbc12676164d
Saruchi11 <saruchi1008@gmail.com>
2026-05-18 10:24:48 +0530

    TryCatch

---
+++ TryCatch.java
@@ -0,0 +1,17 @@
+public class TryCatch {
+    public static void main(String[] args) {
+        try {
+            int a =10;
+            int b =2;
+            int result = a/b;
+            System.out.println("Result:" + result);
+        }
+        catch (ArithmeticException e)   {
+            System.out.println("error occured");
+        }
+        finally {
+            System.out.println("This block will always execute");
+        }
+    }
+
+}
